9edaeae31fdfc8aab74759be40f0d8fdf5978407
Zaimoo <reycezartigley@gmail.com>
2026-01-28 14:57:57 +0800

    fix: conflict for App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import Login from "./pages/Login";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LoginScreen from "./screens/LoginScreen";
+
+import RegisterScreen from "./screens/RegisterScreen";
+
+import HomeScreen from "./screens/HomeScreen";
+
+import ForgotPasswordScreen from "./screens/ForgotPasswordScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Login />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+
+        <Stack.Screen name="Register" component={RegisterScreen} />
+
+        <Stack.Screen name="Home" component={HomeScreen} />
+
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
